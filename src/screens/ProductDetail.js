131605684eb597011afc14edede24f4e8e79aa2d
Saket Zode <55745771+SaketZode@users.noreply.github.com>
2021-04-04 17:34:32 +0530

    integrated redus in project

--- src/screens/ProductDetail.js
+++ src/screens/ProductDetail.js
@@ -1,60 +1,67 @@
 import { Button, Col, Container, Image, ListGroup, Row } from 'react-bootstrap'
-import {React, useState, useEffect} from 'react'
+import {React, useEffect} from 'react'
 import { Link } from 'react-router-dom'
 import Ratings from "../components/Ratings"
 import styled from 'styled-components'
-import axios from 'axios'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProductDetails } from '../actions/ProductListActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 
 function ProductDetail({ match }) {
-    const [product, setProduct] = useState([])
+    const dispatch = useDispatch()
+    const productDetail = useSelector(state => state.productDetailsReducer)
+    const { error, loading, product } = productDetail
 
     useEffect(() => {
-        async function fetchProduct() {
-            const { data } = await axios.get(`/products/${match.params.id}`)
-            setProduct(data)
-        }
-
-        fetchProduct()
-    }, [match.params.id])
+        dispatch(listProductDetails(match.params.id))
+    }, [dispatch, match])
+    
     return (
         <Container>
             <Row>
                 <Link to='/' className='btn btn-outline-primary'>GO BACK</Link>
             </Row>
             <ProductDetailContainer>
-            <Row>
-                <Col sm={6}>
-                    <Image src={product.image} alt={product.name} fluid />
-                </Col>
-                <Col sm={3}>
-                    <ListGroup>
-                        <ListGroup.Item>
-                            <h3>{product.name}</h3>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Ratings ratings={product.rating} reviews={product.numReviews} />
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <p>{product.description}</p>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-                <Col sm={3}>
-                    <ListGroup>
-                        <ListGroup.Item>
-                            <strong>Price: &#8377; {product.price}</strong>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <strong>
-                                Status: {product.countInStock>0 ? 'In Stock' : 'Out of stock!!'}
-                            </strong>
-                        </ListGroup.Item>
-                        <ListGroup.Item>
-                            <Button className='btn-block'>ADD TO CART</Button>
-                        </ListGroup.Item>
-                    </ListGroup>
-                </Col>
-            </Row>
+                {
+                    loading ? <Loader />
+                    : error ? <Message variant='danger'>{error}</Message>
+                    :
+                    <Row>
+                        <Col sm={6}>
+                            <Image src={product.image} alt={product.name} fluid />
+                        </Col>
+                        <Col sm={3}>
+                            <ListGroup>
+                                <ListGroup.Item>
+                                    <h3>{product.name}</h3>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Ratings ratings={product.rating} reviews={product.numReviews} />
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <p>{product.description}</p>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                        <Col sm={3}>
+                            <ListGroup>
+                                <ListGroup.Item>
+                                    <strong>Price: &#8377; {product.price}</strong>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <strong>
+                                        Status: {product.countInStock>0 ? 'In Stock' : 'Out of stock!!'}
+                                    </strong>
+                                </ListGroup.Item>
+                                <ListGroup.Item>
+                                    <Button className='btn-block'>ADD TO CART</Button>
+                                </ListGroup.Item>
+                            </ListGroup>
+                        </Col>
+                    </Row>
+                }
+            
             </ProductDetailContainer>
         </Container>
     )
--- src/screens/ProductList.js
+++ src/screens/ProductList.js
@@ -1,32 +1,38 @@
-import axios from 'axios'
-import { React, useState, useEffect } from 'react'
+import { React, useEffect } from 'react'
 import { Col, Row, Container } from 'react-bootstrap'
+import { useDispatch, useSelector } from 'react-redux'
+import { listProducts } from '../actions/ProductListActions'
+import Loader from '../components/Loader'
+import Message from '../components/Message'
 import Product from '../components/Product'
 
 function ProductList() {
-    const [products, setProducts] = useState([])
-
+    const dispatch = useDispatch()
+    const productList = useSelector(state => state.productReducer)
+    const { error, loading, products } = productList
+    
     useEffect(() => {
-        async function fetchProducts() {
-            const { data } = await axios.get(`/products`)
-            setProducts(data)
-        }
-
-        fetchProducts()
+        dispatch(listProducts())
     }, [])
+
     return (
         <Container>
             <h1>Products</h1>
             <hr/>
-            <Row>
-                
-                { products.map(prod => (
-                    <Col key={ prod.id } sm={12} md={6} lg={4} xl={3}>
-                        <Product product={prod} />
-                    </Col>
-                )) }
-                
-            </Row>
+            {
+                loading ? <Loader />
+                : error ? <Message variant='danger'>{error}</Message>
+                :
+                <Row>
+                    
+                    { products.map(prod => (
+                        <Col key={ prod.id } sm={12} md={6} lg={4} xl={3}>
+                            <Product product={prod} />
+                        </Col>
+                    )) }
+                    
+                </Row>
+            }
         </Container>
     )
 }
